d00f237bc873a5264d0f9bbfa7b7ee3d3d2287c6
Ivan <waizord@yandex.ru>
2021-07-04 02:17:05 +0300

    Initial Commit

---
+++ rs.ios.stage-task7/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  rs.ios.stage-task7
+//
+//  Created by Ivan on 7/4/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ rs.ios.stage-task7/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  rs.ios.stage-task7
+//
+//  Created by Ivan on 7/4/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
