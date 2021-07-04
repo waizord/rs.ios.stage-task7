3c16bee00a8582908dd4d4a3896aa5d5c8c589b4
Ivan <waizord@yandex.ru>
2021-07-04 07:28:11 +0300

    Fix: added all properties from file.xib

--- rs.ios.stage-task7/AuthViewController.m
+++ rs.ios.stage-task7/AuthViewController.m
@@ -8,6 +8,15 @@
 #import "AuthViewController.h"
 
 @interface AuthViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *RSLabel;
+@property (weak, nonatomic) IBOutlet UITextField *loginTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passTextField;
+@property (weak, nonatomic) IBOutlet UIButton *autorizeButton;
+@property (weak, nonatomic) IBOutlet UIView *secureView;
+@property (weak, nonatomic) IBOutlet UILabel *secureLabel;
+@property (weak, nonatomic) IBOutlet UIButton *oneButton;
+@property (weak, nonatomic) IBOutlet UIButton *twoButton;
+@property (weak, nonatomic) IBOutlet UIButton *threeButton;
 
 @end
 
